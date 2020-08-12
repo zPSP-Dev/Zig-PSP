89e84e52e003a418a9e12681d730518b53ea90e6
Nathan <iridescentrosesfall@gmail.com>
2020-08-12 14:16:36 -0400

    CLI

--- tools/sfo/src/main.zig
+++ tools/sfo/src/main.zig
@@ -1,5 +1,40 @@
 const std = @import("std");
+const process = std.process;
 
 pub fn main() !void {
+    std.debug.warn("zSFOTool Utility made by zPSP-Dev!\n\n", .{});
+
+    //Get args
+    var arg_it = process.args();
+    // Skip executable
+    _ = arg_it.skip();
+
+    //Allocator setup
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = &arena.allocator;
+
+    var optionSelected = try (arg_it.next(allocator) orelse{
+        std.debug.warn("Usage: sfotool [read | write] file <...>\n", .{});
+        return;
+    });
+
+    if(std.mem.eql(u8, optionSelected, "-h")){
+        std.debug.warn("Usage: sfotool [read | write] file <...>\n", .{});
+        return;
+    }
     
+    if(std.mem.eql(u8, optionSelected, "-v")){
+        std.debug.warn("Version 1.0\n", .{});
+        return;
+    }
+
+    if(std.mem.eql(u8, optionSelected, "read")){
+        
+    }
+
+    if(std.mem.eql(u8, optionSelected, "write")){
+        
+    }
+
 }
