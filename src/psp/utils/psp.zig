1fbf9af20ff5528d5910d79899bb7dba7d1641c3
Nathan <iridescentrosesfall@gmail.com>
2020-08-26 08:42:36 -0400

    Make a minimalist subset

---
+++ src/psp/utils/psp.zig
@@ -0,0 +1,8 @@
+//Module must always be first!
+pub usingnamespace  @import("module.zig");
+pub usingnamespace  @import("mem-fix.zig");
+pub usingnamespace  @import("constants.zig");
+pub const debug =   @import("debug.zig");
+pub const utils =   @import("utils.zig");
+pub const vram =    @import("vram.zig");
+pub usingnamespace  @import("allocator.zig");
