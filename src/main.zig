774363e438c89bd354af4b48f07576638ef72112
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-10-14 02:36:49 -0400

    PSP ATRAC Safety Wrapper

---
+++ src/main.zig
@@ -0,0 +1,13 @@
+const psp = @import("psp/pspsdk.zig");
+pub const panic = psp.debug.panic;
+
+comptime{
+    asm(psp.module_info("Zig PSP", 0, 1, 0));
+}
+
+pub fn main() !void {
+    psp.utils.enableHBCB();
+    psp.debug.screenInit();
+
+    psp.debug.print("Hi!");
+}
