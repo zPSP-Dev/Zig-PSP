21fa6c2c2c4f211e756b8e4cd74514e72d4f273c
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-12-24 00:26:25 -0500

    Minimize build file for example

--- build.zig
+++ build.zig
@@ -1,10 +1,6 @@
 const Builder = @import("std").build.Builder;
 const psp = @import("build-psp.zig");
 
-const std = @import("std");
-const z = @import("std").zig;
-const builtin = @import("builtin");
-
 pub fn build(b: *Builder) void {
     psp.build_psp(b, psp.PSPBuildInfo{
         .path_to_sdk = "",
