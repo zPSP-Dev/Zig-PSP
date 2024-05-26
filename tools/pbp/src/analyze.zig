7c6c068e2e1390ecb3d16a0c7103a4444733f194
Jae B <doogie1012@gmail.com>
2024-05-26 22:08:50 +1000

    update to compile with zig 0.12 and 0.13

--- tools/pbp/src/analyze.zig
+++ tools/pbp/src/analyze.zig
@@ -4,69 +4,70 @@ const mem = std.mem;
 const process = std.process;
 const fs = std.fs;
 
-usingnamespace @import("common.zig");
+const common = @import("common.zig");
+const readHeader = common.readHeader;
+const default_file_names = common.default_file_names;
 
 pub fn analyzePBP() !void {
+    //Allocator setup
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
     //Get args
-    var arg_it = process.args();
+    var arg_it = try process.argsWithAllocator(allocator);
 
     // Skip executable
     _ = arg_it.skip();
     // Skip command
     _ = arg_it.skip();
 
-    //Allocator setup
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-
-    const allocator = &arena.allocator;
-
     //Get our input file - if it doesn't exist - error out
-    var inputName = try (arg_it.next(allocator) orelse{
-        std.debug.warn("Usage: pbptool analyze <input.pbp>\n\n", .{});
+    const inputName = arg_it.next() orelse {
+        std.log.warn("Usage: pbptool analyze <input.pbp>\n\n", .{});
         return;
-    });
+    };
 
-    if(std.mem.eql(u8, inputName, "-h")){
-        std.debug.warn("Usage: pbptool analyze <input.pbp>\n", .{});
+    if (std.mem.eql(u8, inputName, "-h")) {
+        std.log.warn("Usage: pbptool analyze <input.pbp>\n", .{});
         return;
     }
 
     //Open File
-    var inFile = try fs.cwd().openFile(inputName, fs.File.OpenFlags{.read = true});
+    var inFile = try fs.cwd().openFile(inputName, fs.File.OpenFlags{ .mode = .read_only });
     defer inFile.close();
 
     //Get header
-    var header = try readHeader(inFile);
-    var size = try inFile.getEndPos();
+    const header = try readHeader(inFile);
+    const size = try inFile.getEndPos();
 
     //Print entries
-    std.debug.warn("PBP Entry Table: \n",.{});
+    std.log.warn("PBP Entry Table: \n", .{});
 
-    var i : usize = 0;
-    while(i < 8) : (i += 1){
-        var calcSize : usize = 0;
+    var i: usize = 0;
+    while (i < 8) : (i += 1) {
+        var calcSize: usize = 0;
 
-        if(i + 1 == 8){
+        if (i + 1 == 8) {
             calcSize = size - header.offset[7];
-        }else{
+        } else {
             calcSize = header.offset[i + 1] - header.offset[i];
         }
 
-        if(calcSize == 0){
-            std.debug.warn("\t{s}: \tNOT PRESENT\n", .{default_file_names[i]});
-        }else{
-            std.debug.warn("\t{s}: \tOFFSET {} \t SIZE {}\n", .{default_file_names[i], header.offset[i], calcSize});
+        if (calcSize == 0) {
+            std.log.warn("\t{s}: \tNOT PRESENT\n", .{default_file_names[i]});
+        } else {
+            std.log.warn("\t{s}: \tOFFSET {} \t SIZE {}\n", .{ default_file_names[i], header.offset[i], calcSize });
         }
     }
-    std.debug.warn("PBP Entry Table End\n\n", .{});
+    std.log.warn("PBP Entry Table End\n\n", .{});
 
     //Read Version
     try inFile.seekTo(4);
-    var ver_maj = inFile.reader().readIntNative(u16);
-    var ver_min = inFile.reader().readIntNative(u16);
+    const ver_maj = inFile.reader().readInt(u16, .little);
+    const ver_min = inFile.reader().readInt(u16, .little);
 
     //Finish Print
-    std.debug.warn("PBP Version: {}.{}\n", .{ver_maj, ver_min});
-    std.debug.warn("Size: {} bytes.\n", .{size});
+    std.log.warn("PBP Version: {}.{}\n", .{ ver_maj, ver_min });
+    std.log.warn("Size: {} bytes.\n", .{size});
 }
--- tools/pbp/src/common.zig
+++ tools/pbp/src/common.zig
@@ -7,48 +7,48 @@ const fs = std.fs;
 //The standard header structure for a PBP File
 //This includes a 4 byte signature, followed by the 4 byte version number
 //Then ends with an offset table for its component files.
-pub const PbpHeader = struct{
+pub const PbpHeader = struct {
     signature: [4]u8,
     version: [4]u8,
     offset: [8]u32,
 };
 
 //The default file names of the PBP components
-pub var default_file_names : [8][]const u8 =  [8][]const u8{
-   "PARAM.SFO",
-   "ICON0.PNG",
-   "ICON1.PMF",
-   "PIC0.PNG",
-   "PIC1.PNG",
-   "SND0.AT3",
-   "DATA.PSP",
-   "DATA.PSAR"
+pub const default_file_names: [8][]const u8 = [8][]const u8{
+    "PARAM.SFO",
+    "ICON0.PNG",
+    "ICON1.PMF",
+    "PIC0.PNG",
+    "PIC1.PNG",
+    "SND0.AT3",
+    "DATA.PSP",
+    "DATA.PSAR",
 };
 
 //Reads a PBP Header file
 pub fn readHeader(file: fs.File) !PbpHeader {
-    var header : PbpHeader = undefined;
+    var header: PbpHeader = undefined;
 
     var instream = file.reader();
 
     var bytes_read = try instream.read(header.signature[0..]);
-    
-    if(bytes_read != 4 and std.mem.eql(u8, header.signature[1..], "PBP")){
-        std.debug.warn("Could not read the header!", .{});
+
+    if (bytes_read != 4 and std.mem.eql(u8, header.signature[1..], "PBP")) {
+        std.log.warn("Could not read the header!", .{});
         return error.BadHeader;
     }
 
     bytes_read = try instream.read(header.version[0..]);
-    if(bytes_read != 4){
-        std.debug.warn("Could not read the header!", .{});
+    if (bytes_read != 4) {
+        std.log.warn("Could not read the header!", .{});
         return error.BadHeader;
     }
 
-    var i : usize = 0;
-    while(i < 8) : (i += 1){
-        header.offset[i] = try instream.readIntNative(u32);
+    var i: usize = 0;
+    while (i < 8) : (i += 1) {
+        header.offset[i] = try instream.readInt(u32, .little);
     }
 
-    std.debug.warn("Valid Header!\n\n", .{});
+    std.log.warn("Valid Header!\n\n", .{});
     return header;
 }
