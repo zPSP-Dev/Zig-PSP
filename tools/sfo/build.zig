840a508f14a69cccccc77cfcc011d525c44e436d
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2025-03-26 23:14:36 -0400

    Reimplement SFO Tool, no parse option for now

--- tools/sfo/build.zig
+++ tools/sfo/build.zig
@@ -1,27 +1,38 @@
-const Build = @import("std").Build;
 const std = @import("std");
 
-pub fn build(b: *Build) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{}); // was: std.builtin.Mode.ReleaseFast;
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "sfotool",
+    const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    b.installArtifact(exe);
-    // b.getInstallStep().dependOn(&b.addInstallArtifact(
-    //     exe,
-    //     .{
-    //         .dest_dir = .{ .override = .{
-    //             .custom = "../bin/",
-    //         } },
-    //     },
-    // ).step);
 
-    const run = b.step("run", "Run the application");
+    const exe = b.addExecutable(.{
+        .name = "zSFOTool",
+        .root_module = exe_mod,
+    });
+
+    b.installArtifact(exe);
+
     const run_cmd = b.addRunArtifact(exe);
-    run.dependOn(&run_cmd.step);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const exe_unit_tests = b.addTest(.{
+        .root_module = exe_mod,
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_exe_unit_tests.step);
 }
